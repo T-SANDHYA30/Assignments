1662b4bff636727e26f20b39f9341dc41a011d57
T-SANDHYA30 <thummalurusandhya456@gmail.com>
2025-11-24 13:08:53 +0530

    StringsMethodreferences.java

--- nov24/src/main/java/com/methodreferences/nov24/Strings.java
+++ nov24/src/main/java/com/methodreferences/nov24/Strings.java
@@ -1,23 +1,25 @@
 package com.methodreferences.nov24;
 
 import java.util.function.Function;
-import java.util.function.Supplier;
 
 public class Strings {
 	
 	static class stringUtil{
 		
 		public static int strLen(String name) {
-			return strLen(name);
+			return name.length();
 		}
 	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		String name="sandhya";
+		//String name="sandhya";
 		Function<String, Integer> strLenUsingLambda=(name)->stringUtil.strLen(name);
 		Function<String, Integer> strLenUsingmR=stringUtil::strLen;
+		
+		System.out.println(strLenUsingLambda.apply("sandhya"));
+		System.out.println(strLenUsingmR.apply("sandhya"));
 		
 	
 	}
