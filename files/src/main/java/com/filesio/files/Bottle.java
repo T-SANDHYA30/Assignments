3de9e62e095093a6a7df5da0a5bfdedfe4ff95ce
T-SANDHYA30 <thummaluru.sandhya1@mphasis.com>
2025-11-29 14:54:27 +0530

    initial commit

---
+++ files/src/main/java/com/filesio/files/Bottle.java
@@ -0,0 +1,53 @@
+package com.filesio.files;
+
+import java.io.Serializable;
+
+public class Bottle implements Serializable{
+	
+	private int bottleId;
+	private String brand;
+	private double cost;
+	private String color;
+	public Bottle(int bottleId, String brand, double cost, String color) {
+		super();
+		this.bottleId = bottleId;
+		this.brand = brand;
+		this.cost = cost;
+		this.color = color;
+	}
+	public int getBottleId() {
+		return bottleId;
+	}
+	public void setBottleId(int bottleId) {
+		this.bottleId = bottleId;
+	}
+	public String getBrand() {
+		return brand;
+	}
+	public void setBrand(String brand) {
+		this.brand = brand;
+	}
+	public double getCost() {
+		return cost;
+	}
+	public void setCost(double cost) {
+		this.cost = cost;
+	}
+	public String getColor() {
+		return color;
+	}
+	public void setColor(String color) {
+		this.color = color;
+	}
+	public Bottle() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	@Override
+	public String toString() {
+		return "Bottle [bottleId=" + bottleId + ", brand=" + brand + ", cost=" + cost + ", color=" + color + "]";
+	}
+	
+	
+
+}
