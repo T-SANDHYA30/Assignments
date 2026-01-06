22ef8a931708666897c0eaded3be7394f1fd2ce8
T-SANDHYA30 <thummalurusandhya456@gmail.com>
2026-01-06 15:57:31 +0530

    Delete ts directory

--- ts/demoOnEnums.js
+++
@@ -1,9 +0,0 @@
-var status;
-(function (status) {
-    status[status["PENDING"] = 5] = "PENDING";
-    status[status["ACTIVE"] = 6] = "ACTIVE";
-    status[status["APPROVED"] = 7] = "APPROVED";
-    status[status["REJECTED"] = 8] = "REJECTED";
-})(status || (status = {}));
-var txStatus = status.PENDING; //0
-console.log(txStatus);
--- ts/demoOnEnums.ts
+++
@@ -1,6 +0,0 @@
-enum status{
-    PENDING=5,ACTIVE,APPROVED,REJECTED
-}
-
-let txStatus:status=status.PENDING                //0
-console.log(txStatus);
